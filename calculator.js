6d97eb72ea48747c7c9995cb89e38a7d41824d6b
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2023-11-06 14:51:58 -0800

    Create operator functions.

---
+++ calculator.js
@@ -0,0 +1,21 @@
+let firstNumber, secondNumber, operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    return window[operator](firstNumber, secondNumber);
+}
