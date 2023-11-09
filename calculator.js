57dc8a20fec101689acf6d68551bc37c36037fa8
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2023-11-09 14:23:35 -0800

    Implement operate(); make calculator work.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,7 @@
 let firstNumber, secondNumber, operator;
+let displayValue = 0;
+const operators = ['+', '-', '*', '/'];
+const operatorKeys = ['add', 'subtract', 'multiply', 'divide'];
 
 function add(a, b) {
     return a + b;
@@ -17,5 +20,49 @@ function divide(a, b) {
 }
 
 function operate(operator, firstNumber, secondNumber) {
-    return window[operator](firstNumber, secondNumber);
+    const operatorKey = operatorKeys[operators.indexOf(operator)];
+    return window[operatorKey](parseInt(firstNumber), parseInt(secondNumber));
 }
+
+const display = document.querySelector('#display');
+function updateDisplay(symbol) {
+    display.textContent = symbol;
+    displayValue = symbol;
+}
+
+// Update display, store first and second value, and call operate on '='
+function resolveClick(button) {
+    const input = button.currentTarget.textContent;
+    if (input == '=' && secondNumber) {
+        const solution = operate(operator, firstNumber, secondNumber);
+        updateDisplay(solution);
+        firstNumber = solution;
+        secondNumber = null;
+        operator = null;
+    } else if (operators.includes(input) && (!operator)) {
+        operator = input;
+    } else if (operators.includes(input) && operator && secondNumber) {
+        const solution = operate(operator, firstNumber, secondNumber);
+        updateDisplay(solution);
+        firstNumber = solution;
+        secondNumber = null;
+        operator = input;
+    } else if (input == 'clear') {
+
+    } else if (firstNumber && operator && !secondNumber) {
+        secondNumber = input;
+        updateDisplay(secondNumber);
+    }  else if (!secondNumber && (Number.isInteger(+input))) {
+        firstNumber = input;
+        updateDisplay(firstNumber);
+    }
+}
+
+// Adds event listeners to all calculator buttons.
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        resolveClick(event);
+    })
+})
+
